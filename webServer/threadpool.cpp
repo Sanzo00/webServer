147d34d9f5a15f39474334b219b8ef3c3d984f79
Sanzona <twonapom@gmail.com>
2020-08-31 14:00:38 +0100

    add threadpool

--- webServer/threadpool.cpp
+++
@@ -1,9 +0,0 @@
-#include "threadpool.h"
-
-template <typename T>
-bool threadpool<T>::threadpool(int actor_model, connection_pool *connPool, int thread_number, int max_requests)
-    : m_actor_mode(actor_model), m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL), m_connPool(connPool)
-{
-    
-
-}
--- webServer/threadpool.h
+++ webServer/threadpool.h
@@ -5,7 +5,7 @@
 #include <cstdio>
 #include <exception>
 #include <pthread.h>
-#include "lock.h"
+#include "locker.h"
 #include "sql_connection_pool.h"
 
 template <typename T>
@@ -14,7 +14,7 @@ class threadpool
 public:
     threadpool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_requests = 10000);
     ~threadpool();
-    bool apend(T* request, int state);
+    bool append(T* request, int state);
     bool append_p(T* request);
 
 private:
@@ -31,5 +31,86 @@ private:
     void run();
 };
 
-#endif // THREADPOOL_H
+template <typename T>
+threadpool<T>::threadpool(int actor_model, connection_pool *connPool, int thread_number, int max_requests) {
+    m_actor_model = actor_model;
+    m_thread_number = thread_number; 
+    m_max_requests = max_requests;
+    m_threads = NULL; 
+    m_connPool = connPool;
+    
+    if (thread_number <= 0 || max_requests <= 0) throw std::exception();
+    
+    m_threads = new pthread_t[m_thread_number];
+    if (!m_threads) throw std::exception();
 
+    for (int i = 0; i < thread_number; ++i) {
+        if (pthread_create(m_threads+i, NULL, worker, this) != 0) {
+            delete[] m_threads;
+            throw std::exception();
+        }
+
+        if (pthread_detach(m_threads[i])) {
+            delete[] m_threads;
+            throw std::exception();
+        }
+    }
+}
+
+template <typename T>
+threadpool<T>::~threadpool(){
+    delete[] m_threads;
+}
+
+template <typename T>
+bool threadpool<T>::append(T *request, int state){
+    m_queuelocker.lock();
+    
+    if (m_workqueue.size() >= m_max_requests) {
+        m_queuelocker.unlock();
+        return false;
+    }
+    request->m_state = state;
+    m_workqueue.push_back(request);
+    
+    m_queuelocker.unlock();
+    
+    m_queuestat.post();
+    
+    return true;
+}
+
+template <typename T>
+void *threadpool<T>::worker(void *arg) {
+    threadpool *pool = (threadpool*) arg;
+    pool->run();
+    return pool;
+}
+
+template <typename T>
+void threadpool<T>::run() {
+    while (true) {
+        m_queuestat.wait();
+        m_queuelocker.lock();
+        
+        if (m_workqueue.empty()) {
+            m_queuelocker.unlock();
+            continue;
+        }
+        
+        T* request = m_workqueue.front();
+        m_workqueue.pop_front();
+
+        m_queuelocker.unlock();
+
+        if (!request) continue;
+        
+        /*
+            ...
+
+        */
+
+    }
+}
+
+#endif // THREADPOOL_H
